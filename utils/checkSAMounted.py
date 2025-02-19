dec0c3a7b19de0a2b5d60311f09c333a084bbde2
GylingP <gylingpolar@outlook.com>
2025-02-19 14:43:28 +0800

    update

--- utils/checkSAMounted.py
+++ utils/checkSAMounted.py
@@ -2,6 +2,7 @@ from .getPods import get_pods
 
 def check_sa_mounted(sas):
     result = sas
+    print("sas",sas.items())
     # 获取 Pods
     pods, err = get_pods()
     if err:
@@ -11,6 +12,7 @@ def check_sa_mounted(sas):
     # 遍历 Pods 并更新 SA 数据
     for pod in pods:
         sa_key = f"{pod.namespace}/{pod.service_account}"
+        print("sa_key",sa_key)
         if sa_key in result:
             result[sa_key].is_mounted = True
             result[sa_key].sa_pod = pod
